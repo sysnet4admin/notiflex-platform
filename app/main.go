8202be783d39cac80596d4b7b4976e50d9a72759
Hoon Jo <pagaia@hotmail.com>
2026-04-04 18:01:39 +0900

    feat: OTel tracing + CronJob healthcheck (v0.5.0)
    - Add OpenTelemetry SDK with Tempo exporter to /notify
    - Add OTEL_EXPORTER_OTLP_ENDPOINT env to rollouts
    - Add CronJob healthcheck every 5 minutes

--- app/main.go
+++ app/main.go
@@ -11,11 +11,49 @@ import (
 
 	"github.com/IBM/sarama"
 	"github.com/valkey-io/valkey-go"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
-var version = "v0.4.0"
+var version = "v0.5.0"
+
+func initTracer() func() {
+	ctx := context.Background()
+	endpoint := os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	if endpoint == "" {
+		endpoint = "tempo.monitoring.svc.cluster.local:4318"
+	}
+
+	exporter, err := otlptracehttp.New(ctx,
+		otlptracehttp.WithEndpoint(endpoint),
+		otlptracehttp.WithInsecure(),
+	)
+	if err != nil {
+		log.Printf("OTel exporter init failed: %v", err)
+		return func() {}
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("notiflex-api"),
+			semconv.ServiceVersionKey.String(version),
+		)),
+	)
+	otel.SetTracerProvider(tp)
+
+	return func() { tp.Shutdown(ctx) }
+}
 
 func main() {
+	shutdown := initTracer()
+	defer shutdown()
+	tracer := otel.Tracer("notiflex-api")
+
 	hostname, _ := os.Hostname()
 
 	password := os.Getenv("VALKEY_PASSWORD")
@@ -85,7 +123,9 @@ func main() {
 	})
 
 	http.HandleFunc("/notify", func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.Background()
+		ctx, span := tracer.Start(r.Context(), "notify")
+		defer span.End()
+
 		idResult := client.Do(ctx, client.B().Incr().Key("notiflex:id").Build())
 		id, err := idResult.AsInt64()
 		if err != nil {
