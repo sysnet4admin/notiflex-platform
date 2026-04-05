b6a84501fa443d663a56716578939ac495fd1cac
Hoon Jo <pagaia@hotmail.com>
2026-04-05 09:59:37 +0900

    feat: Valkey + CSI + Canary strategy (v0.3.0)

--- app/main.go
+++ app/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"sync/atomic"
+	"time"
+
+	"github.com/valkey-io/valkey-go"
 )
 
-var version = "v0.2.0"
-var counter int64
+var version = "v0.3.0"
+var valkeyClient valkey.Client
 
 func main() {
+	initValkey()
+
 	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/id", idHandler)
 	http.HandleFunc("/notify", notifyHandler)
@@ -24,6 +30,35 @@ func main() {
 	}
 }
 
+func initValkey() {
+	addr := os.Getenv("VALKEY_ADDR")
+	if addr == "" {
+		addr = "valkey-primary.notiflex.svc.cluster.local:6379"
+	}
+
+	password := os.Getenv("VALKEY_PASSWORD")
+	if pwFile := os.Getenv("VALKEY_PASSWORD_FILE"); pwFile != "" {
+		if data, err := os.ReadFile(pwFile); err == nil {
+			password = string(data)
+		}
+	}
+
+	var err error
+	for i := 0; i < 10; i++ {
+		valkeyClient, err = valkey.NewClient(valkey.ClientOption{
+			InitAddress: []string{addr},
+			Password:    password,
+		})
+		if err == nil {
+			log.Printf("Valkey connected to %s", addr)
+			return
+		}
+		log.Printf("Valkey retry %d/10: %v", i+1, err)
+		time.Sleep(3 * time.Second)
+	}
+	log.Fatalf("Valkey connection failed after 10 retries: %v", err)
+}
+
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{
@@ -32,8 +67,27 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func idHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	cmd := valkeyClient.B().Incr().Key("notiflex:id").Build()
+	result := valkeyClient.Do(ctx, cmd)
+	id, _ := result.AsInt64()
+
+	pod := os.Getenv("HOSTNAME")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"id":      id,
+		"pod":     pod,
+		"version": version,
+	})
+}
+
 func notifyHandler(w http.ResponseWriter, r *http.Request) {
-	id := atomic.AddInt64(&counter, 1)
+	ctx := context.Background()
+	cmd := valkeyClient.B().Incr().Key("notiflex:notification").Build()
+	result := valkeyClient.Do(ctx, cmd)
+	id, _ := result.AsInt64()
+
 	pod := os.Getenv("HOSTNAME")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]interface{}{
@@ -41,16 +95,5 @@ func notifyHandler(w http.ResponseWriter, r *http.Request) {
 		"pod":             pod,
 		"version":         version,
 		"status":          "queued",
-	})
-}
-
-func idHandler(w http.ResponseWriter, r *http.Request) {
-	id := atomic.AddInt64(&counter, 1)
-	pod := os.Getenv("HOSTNAME")
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]interface{}{
-		"id":      id,
-		"pod":     pod,
-		"version": version,
 	})
 }
