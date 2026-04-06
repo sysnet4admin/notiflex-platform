1a1e2bce437b614948f7b805e8c69e8a4f40b47b
Hoon Jo <pagaia@hotmail.com>
2026-04-06 11:04:38 +0900

    feat: Valkey 캐시 연동 + replicas:1 (ch6.1)

--- app/main.go
+++ app/main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"sync/atomic"
+	"time"
+
+	"github.com/valkey-io/valkey-go"
 )
 
-var counter int64
+var client valkey.Client
 
 func main() {
 	hostname, _ := os.Hostname()
+
+	addr := os.Getenv("VALKEY_ADDR")
+	if addr == "" {
+		addr = "localhost:6379"
+	}
+	password := os.Getenv("VALKEY_PASSWORD")
+	if pwFile := os.Getenv("VALKEY_PASSWORD_FILE"); pwFile != "" {
+		if data, err := os.ReadFile(pwFile); err == nil {
+			password = string(data)
+		}
+	}
+
+	var err error
+	for i := 0; i < 10; i++ {
+		client, err = valkey.NewClient(valkey.ClientOption{
+			InitAddress: []string{addr},
+			Password:    password,
+		})
+		if err == nil {
+			break
+		}
+		log.Printf("Valkey 연결 재시도 %d/10: %v", i+1, err)
+		time.Sleep(3 * time.Second)
+	}
+	if err != nil {
+		log.Fatalf("Valkey 연결 실패: %v", err)
+	}
+	defer client.Close()
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
@@ -21,11 +52,17 @@ func main() {
 
 	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{"version": "v0.2.0"})
+		json.NewEncoder(w).Encode(map[string]string{"version": "v0.3.0"})
 	})
 
 	http.HandleFunc("/id", func(w http.ResponseWriter, r *http.Request) {
-		id := atomic.AddInt64(&counter, 1)
+		ctx := context.Background()
+		resp := client.Do(ctx, client.B().Incr().Key("notiflex:id").Build())
+		id, err := resp.AsInt64()
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]interface{}{
 			"id":  id,
