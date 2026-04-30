fd9d025da2fd896a5c5ba43681f7af786cbba9b5
Hoon Jo <pagaia@hotmail.com>
2026-04-30 19:42:50 +0900

    ch3.3: rolling update v0.1.0 → v0.1.1

--- app/main.go
+++ app/main.go
@@ -19,7 +19,7 @@ func main() {
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"status": "ok", "version": "v0.1.0"})
+	json.NewEncoder(w).Encode(map[string]string{"status": "ok", "version": "v0.1.1"})
 }
 
 func idHandler(w http.ResponseWriter, r *http.Request) {
